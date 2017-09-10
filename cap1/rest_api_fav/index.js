fe0acd9ebf4150196f6cfb66dce9f04503b1a2bf
Felix Banguera <felixxxiam@gmail.com>
2017-09-10 15:18:09 -0500

    Installed nodemon to run the node script as a daemon and also changed a console just to test it

--- cap1/rest_api_fav/index.js
+++ cap1/rest_api_fav/index.js
@@ -9,5 +9,7 @@ const port = process.env.PORT || 5522;
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.listen(port, () => 
-	console.log('API REST working on http://localhost:'+port));
+app.listen(port, () => {
+	console.log('API REST working on http://localhost:'+port);
+	console.log('Test nodemon');
+})
