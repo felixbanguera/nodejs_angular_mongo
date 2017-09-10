b424fb06666223577a57f8ea2085f3a6fde43560
Felix Banguera <felixxxiam@gmail.com>
2017-09-10 16:26:13 -0500

    Passing params to the requests by url

--- cap1/rest_api_fav/index.js
+++ cap1/rest_api_fav/index.js
@@ -9,7 +9,11 @@ const port = process.env.PORT || 5522;
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+app.get('/test/:test_param', (req, res) => {
+	const test_param = req.params.test_param;
+	res.status(201).send({text: 'test Response!!!', test_param: `${test_param}`});
+});
+
 app.listen(port, () => {
 	console.log('API REST working on http://localhost:'+port);
-	console.log('Test nodemon');
 })
